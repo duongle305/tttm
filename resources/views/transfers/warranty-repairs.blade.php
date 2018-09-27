2afbdca8aab768afe14cc1910bf23294ecd70109
Duong Le <duongrom.it.305@gmail.com>
2018-09-27 21:22:28 +0700

    update

---
+++ resources/views/transfers/warranty-repairs.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title','Điều chuyển bảo hành sửa chữa')
+
+@section('vendor_js')
+@endsection
+
+
+@section('content')
+
+@endsection
+
+
+@section('custom_js')
+
+@endsection
